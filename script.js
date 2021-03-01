1ab26b68c09e640d26d9638dfef748c7a9ae07d5
Phlipper <marzullop73@gmail.com>
2021-02-28 21:56:20 -0500

    set up city weather search variable, weather search on button click

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+// search for city weather when input and button clicked
+        $("#button").on("click", function () {
+          var citySearch = $("#city-search").val();
+        //   then clear box for new search
+        citySearch(citySearch);
+        $("#city-search").val("");
+       
+    
+    
+    
+    
+    });
+      }
